fa9f959e36caebb87e8760721b69f6a63351dcd4
makeev <makeevstranik@gmail.com>
2022-06-10 23:44:46 +0500

    1_1_3 changed after mentor's remarks

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/SQLQuery.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.dao;
+
+public enum SQLQuery {
+    CREATE_TABLE("""
+            CREATE TABLE IF NOT EXISTS users (
+            id BIGINT UNSIGNED AUTO_INCREMENT,
+            name VARCHAR(20),
+            last_name VARCHAR(20),
+            age BIT(8),
+            PRIMARY KEY(id),
+            UNIQUE(id))
+            """),
+    DROP_TABLE("DROP TABLE IF EXISTS users"),
+    CREATE_USER("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)"),
+    DELETE_USER("DELETE FROM users WHERE id = (?)"),
+    GET_ALL_USERS("SELECT * FROM users"),
+    CLEAN_TABLE("TRUNCATE TABLE users");
+    private final String query;
+
+    SQLQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
